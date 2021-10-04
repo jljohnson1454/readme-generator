dc7bece3363d7b97446113085d62a4a11eeab6fc
Ghidorah <jljohnson1454@gmail.com>
2021-10-03 23:10:10 -0500

    added some user inputs

--- Develop/index.js
+++ Develop/index.js
@@ -11,4 +11,51 @@ function init() {}
 
 // Function call to initialize app
 init();
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Your Project Title',
+            message: 'What is the project title?',
+        },
+        {
+            type: 'input',
+            name:'description',
+            message: 'Please write a description of your project: '
+        },
+        {
+            type: 'confirm',
+            name: 'Table of Contents',
+            message: 'Please enter a table of contents (optional)'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation steps (if any)'
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'What is the project being used for?'
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            messagae: "Please list any collaborators or third party assets used: ",
+        },
+        {
+            type: 'list',
+            name: 'License',
+            message: "Choose the licenses used in this project",
+            choices: [
+                
+            ]
+        }
+
+
+        }
+    ])
+}
