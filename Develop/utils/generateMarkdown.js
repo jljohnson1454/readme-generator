4cfd04364e92b21883a5f68382ed5c6d90e9d471
Ghidorah <jljohnson1454@gmail.com>
2021-10-04 22:41:53 -0500

    added badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const findBadge = `![badge](https://img.shields.io/badge/License-${license}-brightgreen)`;
+  if (license == 'No license') {
+    return "";
+  } else {
+    return findBadge;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -30,8 +37,8 @@ function generateMarkdown(data) {
   ${data.credits}
 
   ## License
-  ![badge](https://img.shields.io)
-  ${data.license}
+  ${renderLicenseBadge(data.license)}
+  
 
 `;
 }
---
+++ index.js
@@ -0,0 +1,82 @@
+
+console.log('Hello World')
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./Develop/utils/generateMarkdown');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data)
+
+}
+
+
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the project title? (Required)',
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter project title!');
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name:'description',
+            message: 'Please write a description of your project (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'table',
+            message: 'Please enter a table of contents (optional)'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation steps (if any)'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the project being used for?'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            messagae: "Please list any collaborators or third party assets used: ",
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: "Choose the licenses used in this project",
+            choices: [ 'MIT', 'GNU GPLv3', 'ISC', 'Apache', 'No license'
+
+            ]
+        }
+    ])
+    .then(data => {
+        writeToFile('README.md', generateMarkdown(data));
+    console.log(data)}
+    )
+
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+    promptUser();
+    }
+
+// Function call to initialize app
+init();
